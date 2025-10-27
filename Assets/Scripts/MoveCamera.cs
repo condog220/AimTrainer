35916defac8a54c9b4a588b91fc0f298d267725f
condog220 <103192802+condog220@users.noreply.github.com>
2025-10-27 22:19:01 +0000

    Add pause menu functionality to game
    Implemented a pause menu with Resume and Quit buttons in main game. Added PauseScript to handle pausing via ESC key and toggling the pause menu UI. Updated MoveCamera to disable camera movement when paused.

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -25,6 +25,12 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (PauseScript.isPaused)
+        {
+            return;
+        }
+
         if(locked){
             rotationY += Input.GetAxisRaw("Mouse X") * sensMultiplier/0.4f;
             rotationX -=  Input.GetAxisRaw("Mouse Y") * sensMultiplier/0.4f;
